e2c675eb2b0342e2ed2a5c2ce96e8cb9f1c1f1fc
jayzumen <reinhardtjanniklas96@gmail.com>
2022-08-12 18:10:13 +0200

    changed banner layout

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -21,8 +21,8 @@ function Banner({ netflixOriginals }: Props) {
   }, [netflixOriginals]);
 
   return (
-    <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end'>
-      <div className='absolute top-0 left-0 -z-10 h-[95vh] w-screen'>
+    <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[80%] lg:justify-end lg:pb-12'>
+      <div className='absolute top-0 left-0 -z-10 h-[95vh] w-full'>
         <Image
           src={`${baseUrl}${
             movie?.backdrop_path || movie?.poster_path
@@ -32,21 +32,26 @@ function Banner({ netflixOriginals }: Props) {
         />
       </div>
 
-      <h1 className='text-2xl lg:text-7xl md:text-4xl font-bold'>
-        {movie?.title || movie?.name}
-      </h1>
-      <p className='max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl'>
-        {movie?.overview}
-      </p>
+      <div className='pt-24 lg:pt-52 xl:pt-60'>
+        <div className='md:min-h-[250px] pt-12 md:pt-20 lg:pt-16'>
+          <h1 className='text-2xl lg:text-7xl md:text-4xl font-bold pb-2'>
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <p className='hidden md:flex max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl pb-2'>
+            {movie?.overview}
+          </p>
+        </div>
 
-      <div className='flex space-x-3'>
-        <button className='bannerButton bg-white text-black'>
-          <FaPlay className='h-4 w-4 text-black md:h-7 md:w-7' /> Play
-        </button>
-        <button className='bannerButton bg-[gray]/70'>
-          More Info{" "}
-          <InformationCircleIcon className='h-5 w-5 md:h-7 md:w-7' />
-        </button>
+        <div className='flex space-x-3'>
+          <button className='bannerButton bg-white text-black'>
+            <FaPlay className='h-4 w-4 text-black md:h-7 md:w-7' />{" "}
+            Play
+          </button>
+          <button className='bannerButton bg-[gray]/70'>
+            <InformationCircleIcon className='h-5 w-5 md:h-8 md:w-8' />
+            More Info
+          </button>
+        </div>
       </div>
     </div>
   );
